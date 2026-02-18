2a074574286215eba65e350cf2f49ad8aaeb2b4f
wesferg <wferg@berkeley.edu>
2026-02-18 15:21:46 -0800

    Update WEEK5_Engin11_Lab.py

--- WEEK5_Engin11_Lab.py
+++ WEEK5_Engin11_Lab.py
@@ -52,7 +52,7 @@ meta = ['time', "pm10 standard", "pm25 standard", "pm100 standard",
 csvwriter.writerow(meta)
 
 start = time.time()
-while time.time() < start + 10:
+while time.time() < start + 300:
 
 #for i in range(10):
     time.sleep(2)
